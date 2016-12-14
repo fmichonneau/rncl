e7cc7236eadf9ac1a7e239062ca6e23671ea672f
Francois Michonneau <francois.michonneau@gmail.com>
2016-12-14 21:18:34 +0100

    convert collapse_singles to C++

--- R/collapse_singles.R
+++ R/collapse_singles.R
@@ -2,7 +2,7 @@
 ## written by Emmanuel Paradis
 ## I included here a copy of it as the current testing version of ape 3.3.0.5
 ## has a bug in it. This version seems to work for rncl at this stage.
-collapse_singles <- function(tree) {
+collapse_singles_old <- function(tree) {
     elen <- tree$edge.length
     xmat <- tree$edge
     node.lab <- tree$node.label
@@ -30,5 +30,38 @@ collapse_singles <- function(tree) {
     tree$edge.length <- elen
     tree$node.label <- node.lab
     tree$Nnode <- nnode
+    tree
+}
+
+
+collapse_singles <- function(tree) {
+
+    if (is.null(tree$edge.length)) {
+        elen <- numeric(0)
+    } else {
+        elen <- tree$edge.length
+    }
+
+    res <- collapse_single_cpp(
+        ances = tree$edge[, 1],
+        desc = tree$edge[, 2],
+        elen = elen,
+        nnode = tree$Nnode
+    )
+
+    new_mat <- matrix(c(res$ances, res$desc), ncol = 2)
+    tree$edge <- new_mat
+    tree$edge.length <- res$edge.length
+    tree$Nnode <- res$Nnode
+
+    if (!is.null(tree$node.label)) {
+        idx_nd_lbl <- res$position_singletons - (length(tree$tip.labels) - 1)
+
+        warning("Dropping singleton nodes with labels: ",
+                paste(tree$node.label[idx_nd_lbl], collapse = ", "))
+
+        tree$node.label <- tree$node.label[- idx_nd_lbl]
+    }
+
     tree
 }
---
+++ src/collapse_singles.cpp
@@ -0,0 +1,122 @@
+#include <Rcpp.h>
+#include <iostream>
+#include <set>
+#include <algorithm>
+
+std::vector<int> tabulate_tips (Rcpp::IntegerVector ances) {
+// tabulates ancestor nodes that are not the root.
+    int n = Rcpp::max(ances);
+    std::vector<int> ans(n);
+    for (int i=0; i < ances.size(); i++) {
+        int j = ances[i];
+        if (j > 0) {
+            ans[j - 1]++;
+        }
+    }
+    return ans;
+}
+
+bool is_one(int i) { return ( i == 1); }
+
+//[[Rcpp::export]]
+int n_singletons (Rcpp::IntegerVector ances) {
+    std::vector<int> tab_tips = tabulate_tips(ances);
+    int j = count_if (tab_tips.begin(), tab_tips.end(), is_one);
+    return j;
+}
+
+
+Rcpp::IntegerVector which_integer(Rcpp::IntegerVector x, Rcpp::IntegerVector yInt) {
+  Rcpp::IntegerVector v = Rcpp::seq(0, x.size()-1);
+  int y(1);
+  y = yInt[0];
+  return v[x == y];
+}
+
+Rcpp::IntegerVector match_and_substract(Rcpp::IntegerVector x, Rcpp::IntegerVector yInt) {
+    int y(1);
+    y = yInt[0];
+    for (unsigned k=0; k < x.size(); ++k) {
+	if (x[k] > y)
+	    x[k] = x[k] - 1;
+    }
+    return x;
+}
+
+std::vector<int> match_and_substract (std::vector<int> x, int y) {
+    for (unsigned k=0; k < x.size(); ++k) {
+ 	if (x[k] > y)
+ 	    x[k] = x[k] - 1;
+     }
+     return x;
+}
+
+
+//[[Rcpp::export]]
+Rcpp::List collapse_single_cpp(
+    Rcpp::IntegerVector ances,
+    Rcpp::IntegerVector desc,
+    Rcpp::NumericVector elen,
+    Rcpp::NumericVector nnode) {
+
+    int n_singles = n_singletons(ances);
+
+    std::vector<int> tab_node = tabulate_tips(ances);
+    Rcpp::IntegerVector tab_node_rcpp(tab_node.size());
+    tab_node_rcpp = tab_node;
+    Rcpp::IntegerVector position_singleton = which_integer(tab_node_rcpp, Rcpp::IntegerVector::create(1));
+    Rcpp::IntegerVector position_singleton_orig = position_singleton;
+
+    while (position_singleton.size() > 0) {
+	// Rcpp::Rcout << "tabNode is ";
+	//     for (unsigned k = 0; k < tabNode.size(); ++k)
+	// 	Rcpp::Rcout << " " << tabNode[k];
+	// Rcpp::Rcout << std::endl;
+	// Rcpp::Rcout << "position singleton is ";
+	// for (unsigned k = 0; k < positionSingleton.size(); ++k)
+	//     Rcpp::Rcout << " " << positionSingleton[k];
+	// Rcpp::Rcout << std::endl;
+	int i = position_singleton[0];
+	Rcpp::IntegerVector iV(1);
+	iV = i;
+	//Rcpp::Rcout << "i is " << i << " and iV is " << iV << std::endl;
+	Rcpp::IntegerVector prev_node = which_integer(desc, iV + 1);
+	Rcpp::IntegerVector next_node = which_integer(ances, iV + 1);
+	//Rcpp::Rcout << "prev_node is " << prev_node << " and next_node is " << next_node << std::endl;
+	//Rcpp::Rcout << "before desc:";
+	//for (unsigned k = 0; k < desc.size(); ++k)
+	//    Rcpp::Rcout << " " << desc[k];
+	//Rcpp::Rcout << std::endl;
+	desc[prev_node] = desc[next_node];
+	//Rcpp::Rcout << "after desc:";
+	//for (unsigned k = 0; k < desc.size(); ++k)
+	//    Rcpp::Rcout << " " << desc[k];
+	//Rcpp::Rcout << std::endl;
+	Rcpp::IntegerVector to_rm = which_integer(ances, iV + 1);
+	//Rcpp::Rcout << "to_rm is " << to_rm << std::endl;
+	desc.erase(to_rm[0]);
+	ances.erase(to_rm[0]);
+	desc = match_and_substract(desc, iV);
+	ances = match_and_substract(ances, iV);
+	nnode = nnode - 1;
+
+	if (elen.size() > 0) {
+	    elen[prev_node] = elen[prev_node] + elen[next_node];
+	    elen.erase(next_node[0]);
+	}
+
+	tab_node = tabulate_tips(ances);
+	tab_node_rcpp(tab_node.size());
+	tab_node_rcpp = tab_node;
+	position_singleton = which_integer(tab_node_rcpp, Rcpp::IntegerVector::create(1));
+    }
+
+    Rcpp::List res = Rcpp::List::create(
+	Rcpp::Named("ances") = ances,
+	Rcpp::Named("desc") = desc,
+	Rcpp::Named("Nnode") = nnode,
+	Rcpp::Named("edge.length") = elen,
+	Rcpp::Named("position_singletons") = position_singleton_orig);
+
+    return res;
+}
